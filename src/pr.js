d404dd55cf3fec8a9039a4bdd7f19ae5d61f1974
Michael Lahr <michael.lahr@gmail.com>
2024-03-22 11:18:28 +0800

    update

--- src/pr.js
+++ src/pr.js
@@ -100,28 +100,29 @@ async function pr() {
         .map(s => s.trim())
 
     axios.defaults.headers.common['X-API-Key'] = core.getInput('API_KEY')
-    const response = await axios.post(
-        'https://www.codexanalytica.com/api/v0/comment',
-        {
+    const response = axios
+        .post('https://www.codexanalytica.com/api/v0/comment', {
             git_diff: diff,
             pull_request: prDetails,
             exclude_patterns: excludePatterns,
             include_patterns: includePatterns
-        }
-    )
-    for (const review of response.data) {
-        try {
-            await createReviewComment(
-                octokit,
-                prDetails.owner,
-                prDetails.repository,
-                prDetails.pull_number,
-                review['reviews']
-            )
-        } catch (e) {
-            core.setFailed(`${e}: ${JSON.stringify(review)}`)
-        }
-    }
+        })
+        .then(response => {
+            for (const review of response.data) {
+                try {
+                    createReviewComment(
+                        octokit,
+                        prDetails.owner,
+                        prDetails.repository,
+                        prDetails.pull_number,
+                        review['reviews']
+                    ).catch(e => core.setFailed(e))
+                } catch (e) {
+                    core.setFailed(`${e}: ${JSON.stringify(review)}`)
+                }
+            }
+        })
+        .catch(e => core.setFailed(e))
 }
 
 export default pr
